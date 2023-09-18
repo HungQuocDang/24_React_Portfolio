f91fa344c52061d2b3aade004740cb4dbd6aa3c3
HungQuocDang <129162404+HungQuocDang@users.noreply.github.com>
2023-09-17 21:00:10 -0400

    update

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,14 +3,11 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About me</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      Graduating from Edx Bootcamp at Carleton University as a Full-Stack Web Developer which is in a very high demands of Web developers.
+      I will be graduating this October 2023 and will be very excited to work in the field of Full-Stack Web Development.
+
       </p>
     </div>
   );
